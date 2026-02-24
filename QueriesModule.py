47d435f80e6026117bc6aaebb0a5f9b57b6793d5
Globox <globox610@hotmail.com>
2026-02-24 19:48:19 +0100

    Groundwork for python connection to DB

--- QueriesModule.py
+++ QueriesModule.py
@@ -1,11 +1,117 @@
-import os
-import dotenv
-import mysql.connector
+from mysql.connector.connection import MySQLConnection
+from mysql.connector import Error as MySQLError
 
 
+def deletePlayer(conn: MySQLConnection) -> None:
+    player_id = int(input("Choose id of player to delete ").strip())
+
+    selectSQL = """
+    SELECT *
+    FROM player_profile
+    WHERE playerID = %s
+    """
+
+    deleteSQL = """
+    DELETE FROM player_profile
+    WHERE playerID = %s
+    """
+
+    cur = conn.cursor(dictionary=True)
+    try:
+        cur.execute(selectSQL, (player_id,))
+        row = cur.fetchone()
+        if row is None:
+            print("No matching player found")
+            return
+        choice = input(
+            f"You are about to delete following player from database: {row['username']}. Proceed? Y/N "
+        ).upper()
+        if choice != "Y":
+            print("Delete cancelled")
+            return
+
+        cur.execute(deleteSQL, (player_id,))
+        conn.commit()
+
+        if cur.rowcount == 0:
+            print("Player was not deleted (no longer existed)")
+        else:
+            print("Player deletion successful")
+
+    except MySQLError as error:
+        conn.rollback()
+        print(f"Error during insert {error}")
+        raise
+    finally:
+        cur.close()
 
 
-def addPlayer():
-    # INSERT INTO player_profile(username, email, dob)
-    # VALUES (%s, %s, %s)
+def addPlayer(conn: MySQLConnection):
+
+    username = input("Choose player username ").strip()
+    email = input("Input player email ").strip()
+    dob = input("Input player dob YYYY-MM-DD ").strip()
+    choice = input(
+        f"{username}, {email}, {dob} about to be added, proceed (Y/N)?"
+    ).upper()
+    if choice != "Y":
+        print("Aborting...")
+        return
+    if not username:
+        print("Username invalid")
+        return 0
+    if not email:
+        print("Email invalid")
+        return 0
+    if not dob:
+        print("Date of Birth invalid")
+        return 0
+
+    insertSQL = """
+    INSERT INTO player_profile (username, email, dob)
+    VALUES (%s, %s, %s)
+    """
+
+    selectSQL = """
+    SELECT playerID, username, email, dob
+    FROM player_profile
+    WHERE playerID = %s
+    """
+
+    cur = conn.cursor(dictionary=True)
+    try:
+        cur.execute(insertSQL, (username, email, dob))
+        createdID = cur.lastrowid
+        conn.commit()
+
+        cur.execute(selectSQL, (createdID,))
+        row = cur.fetchone()
+        return row
+    except MySQLError as error:
+        conn.rollback()
+        print(f"Error during insert: {error}")
+        raise
+    finally:
+        cur.close()
+
+
+def showAllPlayers(conn: MySQLConnection):
+
+    sql = """
+    SELECT *
+    FROM player_profile
+    """
+    cur = conn.cursor(dictionary=True)
+    try:
+        cur.execute(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print(f"ID: {row['playerID']} username: {row['username']}")
+
+    except MySQLError as error:
+        print(error)
+
+    finally:
+        cur.close()
+
     return
--- main.py
+++ main.py
@@ -2,48 +2,61 @@ import test_db as tb
 import QueriesModule as QM
 import os
 import dotenv
-import mysql.connector
+import mysql.connector as CON
 
 
 def menuPrompt():
-    print("Hello, welcome to the player data tracker. Please choose one of the following:\n" \
-    "1. List all players\n" \
-    "2. Add Match to player\n" \
-    "3. Look at specific player\n"\
-    "4. Test connection"
+    print(
+        "Hello, welcome to the player data tracker. Please choose one of the following:\n"
+        "1. List all players\n"
+        "2. Add player\n"
+        "3. Look at specific player\n"
+        "4. Delete specific player\n"
+        "5. Test connection\n"
+        "6. Quit"
     )
-            
-            
-            
 
+
+def startConnection():
+    dotenv.load_dotenv()
+    if not os.getenv("DB_HOST"):
+        raise RuntimeError("DB_HOST is missing in env file or not reached")
+    return CON.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
 
 
 def main():
     running = True
-    #print("CWD:", os.getcwd())
-    env_path = dotenv.find_dotenv(usecwd=True)
-    print("find_dotenv:", repr(env_path))
+    conn = startConnection()
 
-    """while (running):
+    while running:
         menuPrompt()
         userInput = input()
-        match(userInput):
-            case("1"):
-                break
-            case("2"):
-                
-                break
-            
-            case("3"):
-                
-                break
-            case("4"):
+        match userInput:
+            case "1":
+                QM.showAllPlayers(conn)
+            case "2":
+                addedPlayer = QM.addPlayer(conn)
+                if addedPlayer is not None:
+                    print(
+                        f"username {addedPlayer['username']}, email {addedPlayer['email']}, dob {addedPlayer['dob']}"
+                    )
+            case "3":
+                QM
+            case "4":
+                QM.deletePlayer(conn)
+            case "5":
                 tb.testConnection()
-                break
-"""
+            case "6":
+                running = False
+
+    conn.close()
+
 
 if __name__ == "__main__":
     main()
-
-
-
