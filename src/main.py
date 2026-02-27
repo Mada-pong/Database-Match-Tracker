1b735e7449d14876345cf2bf8587d899b44eb231
Mada-pong <adam.nguyen015@gmail.com>
2026-02-27 16:36:34 +0100

    Fixed router not being found in the right folder

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from src.routers import matches
 
 app = FastAPI()
+app.include_router(matches.router)
 
 @app.get("/")
 def home():
     return {"message": "Hello, FastAPI!"}
-
-class UserInput(BaseModel):
-    text: str 
-
-@app.post("/submit")
-def submit_text(data: UserInput):
-    print(type(data))
-    print(data.text)
-    return {"you_sent": data.text}
--- src/routers/matches.py
+++ src/routers/matches.py
@@ -1,7 +1,18 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
 
 router = APIRouter(prefix="/matches", tags=["matches"])
+
+class UserInput(BaseModel):
+    text: str 
 
 @router.post("/")
 def create_match():
     return {"status": "ok"}
+
+@router.post("/submit")
+def submit_text(data: UserInput):
+    print(type(data))
+    print(data.text)
+    return {"you_sent": data.text}
