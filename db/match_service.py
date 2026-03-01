fad75534ad20737e3bece654d3511f931eb9d504
Mada-pong <adam.nguyen015@gmail.com>
2026-03-01 17:21:30 +0100

    Added player being shown when searching for a match and correctly show a player as inactive when it has been deleted

--- db/match_service.py
+++ db/match_service.py
@@ -6,7 +6,7 @@ class Side(Enum):
     RED = 1 
     BLUE = 2
 
-def get_Match_By_ID(conn: MySQLConnection, matchID: int) -> dict:
+def get_Match_By_ID(matchID: int, conn: MySQLConnection) -> dict:
     cur = conn.cursor(dictionary=True)
     
     selectSQL = """
@@ -24,9 +24,37 @@ def get_Match_By_ID(conn: MySQLConnection, matchID: int) -> dict:
         print(f"MYSQL ERROR: \n {error}")
     finally:
         cur.close()
+        
+def get_Match_Related_Player_By_ID(matchID: int, conn: MySQLConnection):
+    cur = conn.cursor(dictionary=True)
+    
+    selectSQL = """
+    select player_profile.playerID, 
+    CASE 
+        WHEN player_profile.isActive = FALSE THEN 'Inactive'
+        ELSE player_profile.username
+    END AS username, 
+    side, kills, deaths, assists from match_player 
+    left join player_profile
+    on match_player.playerID = player_profile.playerID
+    where match_player.matchID = %s
+    order by side asc;
+    """
+    
+    args = (matchID,)
+    
+    try: 
+        cur.execute(selectSQL, args)
+        rows = cur.fetchall()
+        return rows
+    except MySQLError as error:
+        print(f"MYSQL ERROR: \n {error}")
+    finally:
+        cur.close()
+        
 
 ### Add a match and returns the match ID for later use. 
-def add_Match(conn: MySQLConnection, start_datetime: str, end_datetime: str, winning_side: Side) -> int:
+def add_Match(start_datetime: str, end_datetime: str, winning_side: Side, conn: MySQLConnection) -> int:
     cur = conn.cursor()
     args = (start_datetime, end_datetime, winning_side.name.capitalize(), 0)
     matchID = -1
@@ -43,7 +71,7 @@ def add_Match(conn: MySQLConnection, start_datetime: str, end_datetime: str, win
         
     return matchID
 
-def add_Player_To_Match(conn: MySQLConnection, matchID: int, playerID: int, side: Side, kills: int, deaths: int, assists: int):
+def add_Player_To_Match(matchID: int, playerID: int, side: Side, kills: int, deaths: int, assists: int, conn: MySQLConnection):
     cur = conn.cursor()
     
     insertSQL = """
--- src/routers/matches.py
+++ src/routers/matches.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from src.models.match_entry import MatchEntry
-from db.match_service import get_Match_By_ID, add_Match, add_Player_To_Match, Side
+from db.match_service import get_Match_By_ID, add_Match, add_Player_To_Match, Side, get_Match_Related_Player_By_ID
 from db.connection_service import get_connection
 from mysql.connector import MySQLConnection
 
@@ -8,16 +8,18 @@ router = APIRouter(prefix="/matches", tags=["matches"])
 
 @router.get("/")
 def get_match(matchID: int, conn: MySQLConnection = Depends(get_connection)):
-    match_data = get_Match_By_ID(conn, matchID)
+    match_data = get_Match_By_ID(matchID, conn)
+    player_match_data = get_Match_Related_Player_By_ID(matchID=matchID, conn=conn)
     
     return {"status": "ok",
-            "data": match_data}
+            "match_data": match_data,
+            "player_data": player_match_data}
 
 @router.post("/SubmitMatch")
 def create_match(data: MatchEntry, conn: MySQLConnection = Depends(get_connection)):
-    new_matchID = add_Match(conn, data.start_datetime, data.end_datetime, data.winning_side)
+    new_matchID = add_Match(data.start_datetime, data.end_datetime, data.winning_side, conn)
     
     for player in data.player_list:
-        add_Player_To_Match(conn, new_matchID, player.player_id, player.side, player.kills, player.deaths, player.assists)
+        add_Player_To_Match(new_matchID, player.player_id, player.side, player.kills, player.deaths, player.assists, conn)
         
     return {"Status": "ok"}
